36153cd276343b68c9ac399349411bf6c9997dda
Jennyfer2004 <jennifer.ss2004@gmail.com>
2026-01-01 20:13:23 -0500

    add the modifec to the jobs

--- aw-dwh/projects/dagster/adventureworks_orchestration/jobs.py
+++ aw-dwh/projects/dagster/adventureworks_orchestration/jobs.py
@@ -4,7 +4,7 @@ from .constants import *
 sample_job = dg.define_asset_job(
         "sample_job",
         selection=dg.AssetSelection.groups(ASSET_GROUP_LABS),
-        executor_def=dg.multiprocess_executor.configured({"max_concurrent": 2}),
+        executor_def=dg.multiprocess_executor.configured({"max_concurrent": 6}),
     )
 
 five_minutes_schedule = dg.ScheduleDefinition(
@@ -15,25 +15,25 @@ five_minutes_schedule = dg.ScheduleDefinition(
 landing_job =  dg.define_asset_job(
     "landing_assets_job",
     selection=dg.AssetSelection.groups(ASSET_GROUP_LANDING),
-    executor_def=dg.multiprocess_executor.configured({"max_concurrent": 2}),
+    executor_def=dg.multiprocess_executor.configured({"max_concurrent": 6}),
 )
 
 bronze_job =  dg.define_asset_job(
     "bronze_assets_job",
     selection=dg.AssetSelection.groups(ASSET_GROUP_BRONZE),
-    executor_def=dg.multiprocess_executor.configured({"max_concurrent": 1}),
+    executor_def=dg.multiprocess_executor.configured({"max_concurrent": 6}),
 )
 
 silver_job =  dg.define_asset_job(
     "silver_assets_job",
     selection=dg.AssetSelection.groups(ASSET_GROUP_SILVER),
-    executor_def=dg.multiprocess_executor.configured({"max_concurrent": 2}),
+    executor_def=dg.multiprocess_executor.configured({"max_concurrent": 6}),
 )
 
 gold_job = dg.define_asset_job(
     "gold_assets_job",
     selection=dg.AssetSelection.groups(ASSET_GROUP_GOLD),
-    executor_def=dg.multiprocess_executor.configured({"max_concurrent": 2}),
+    executor_def=dg.multiprocess_executor.configured({"max_concurrent": 6}),
 )
 
 jobs = [sample_job, landing_job, bronze_job, silver_job, gold_job]
